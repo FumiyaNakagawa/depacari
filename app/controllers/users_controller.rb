af0839fd600ac0d99b91826805ad884e4f226640
Fumiya Nakagawa <nakagawa@dptr.jp>
2019-11-19 18:03:16 +0900

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,37 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
   def index
+  end
 
+  def new
+    @user = User.new
+
+    render layout: "no_search"
+    
   end
 
   def create
-    render :layout => "no_search"
     @user = User.new(user_params)
     if @user.save
-      render html: "create user"
+      puts 'true'
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
-      render 'create'
+      puts 'false'
+      render :new, layout: "no_search"
     end
+  end
+
+  def show
   end
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
-    params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:user_name, :password, :password_confirmation, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'users', to: 'users#index'
-  get 'users/create'
-  get 'users/login'
+  # get 'users', to: 'users#index'
+  # post 'users/create'
+  # get 'users/login'
+  resources :users
 end
