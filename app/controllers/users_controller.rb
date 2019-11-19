e78447f5fcd5449cd56314582bcaeec5cc4237d9
Fumiya Nakagawa <nakagawa@dptr.jp>
2019-11-19 12:43:30 +0900

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new()
+    render :layout => "no_search"
+    @user = User.new(user_params)
+    if @user.save
+      render html: "create user"
+    else
+      render 'create'
+    end
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
+  end
 end
--- db/migrate/20191114070624_create_users.rb
+++ db/migrate/20191114070624_create_users.rb
@@ -2,9 +2,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :user_name
-      t.string :password
       t.integer :phone_number
-      t.string :mail
+      t.string :email
       t.string :name
       t.string :address
       t.integer :evaluation_point
