a2d1afdc294dee5d8d0925c047f2f8e88cac1e4b
Fumiya Nakagawa <nakagawa@dptr.jp>
2019-11-20 18:52:12 +0900

    create login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,28 @@
 class SessionsController < ApplicationController
   def new
+    render layout: "no_search"
+  end
+
+  def create
+    puts params[:email]
+    # ログイン処理を書いてリダイレクトする
+    user = User.find_by(email: params[:email].downcase)
+    puts "start"
+    puts params
+    puts "end"
+    if user && user.authenticate(params[:password])
+      puts 'true'
+      log_in user
+      redirect_to user
+    else
+    # エラーメッセージを作成する
+    flash.now[:danger] = 'ログインに失敗しました'
+    puts 'false'
+    render :new, layout: "no_search"
+    end
+  end
+  
+  def destroy
+
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+  def log_in(zduser)
+    session[:user_id] = user.id
+  end
+
+  def crrent_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
