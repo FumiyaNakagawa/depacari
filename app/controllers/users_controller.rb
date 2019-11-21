3663729f439def4f600fa409b155979dce1bbd44
Fumiya Nakagawa <nakagawa@dptr.jp>
2019-11-21 17:58:31 +0900

    ss

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :corrent_user, only: [:edit, :update]
   
   def index
   end
@@ -23,6 +25,20 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if@user.update_attributes(user_params)
+      # flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def set_user
@@ -30,5 +46,20 @@ class UsersController < ApplicationController
   end
   def user_params
     params.require(:user).permit(:user_name, :password, :password_confirmation, :email)
+  end
+
+  # ログイン済ユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'ログインしてください'
+      redirect_to login_url
+    end
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
