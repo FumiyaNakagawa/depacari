1c6c8d5b6940d46fbedd65bfc5e0cb74eb99225c
Fumiya Nakagawa <nakagawa@dptr.jp>
2019-11-21 11:44:09 +0900

    eferera

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def crrent_user
+  def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
