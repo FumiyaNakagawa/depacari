b909c33e746473d9f6c0d5edbb3cf5e834d6f827
Fumiya Nakagawa <nakagawa@dptr.jp>
2019-11-25 17:30:29 +0900

    creat views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,28 @@
 class ProductsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @products = @user.products.paginate(page: params[:page])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_products
+  end
+
+  def product_params
+  end
 end
