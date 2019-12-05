c522af4f353b82d00ed0a30f4dd4753a932062cb
Fumiya Nakagawa <nakagawa@dptr.jp>
2019-12-05 12:03:26 +0900

    fix input type number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_065053) do
+ActiveRecord::Schema.define(version: 2019_12_05_023136) do
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "product_images", force: :cascade do |t|
     t.integer "product_id"
