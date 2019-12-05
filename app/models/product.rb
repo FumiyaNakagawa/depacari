2b6b616ba9bed69712f39f7dba7a691d34ad3455
Fumiya Nakagawa <nakagawa@dptr.jp>
2019-12-05 15:37:37 +0900

    fix input type num

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,10 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :product_images, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
-  validates :user_id, presence: true
   validates :name, presence: true, length: { maximum:50 }
+  validates :details, length: { maximum:255 }
+  validates :price, presence: true, length: { maximum:20 }
+  validates :condition, presence: true
 
   accepts_nested_attributes_for :product_images, allow_destroy: true
 end
