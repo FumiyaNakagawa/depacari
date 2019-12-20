a15e38ff6764ad3de2bc97f687286ad30e774659
Fumiya Nakagawa <nakagawa@dptr.jp>
2019-12-20 19:23:26 +0900

    create order_spec

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,7 +11,7 @@ class Order < ApplicationRecord
   enum status: { waiting: 0 }
 
   validates :payment_method, presence: true, inclusion: { in: %w[クレジットカード depacariポイントで購入] }
-  validates :use_depacari_point, presence: true, length: { maximum: 20 }
+  validates :use_depacari_point, presence: true, numericality: {less_than: 100000000	}
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
 end
