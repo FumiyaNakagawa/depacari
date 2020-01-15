599b497e049ca2ccd53bdde557ed601edd2542d9
Fumiya Nakagawa <nakagawa@dptr.jp>
2020-01-15 19:53:31 +0900

    Add before action profile controller

--- app/controllers/user/profiles_controller.rb
+++ app/controllers/user/profiles_controller.rb
@@ -1,24 +1,22 @@
 # frozen_string_literal: true
-
 class User::ProfilesController < ApplicationController
+  before_action :set_user
   before_action :logged_in_user
   def index; end
 
   def edit
-    @user = User.find(current_user.id)
   end
 
   def edit_address
     # http://localhost:3000/user/profiles/edit_address?next_url=/user/products/28/orders/new
-    @user = User.find(current_user.id)
+
   end
 
   def edit_password
-    @user = User.find(current_user.id)
   end
 
   def update
-    @user = User.find(current_user.id)
+
     if @user.update_attributes(update_user_params)
       # flash[:success] = 'Profile updated'
       redirect_back(fallback_location: root_path)
@@ -29,6 +27,10 @@ class User::ProfilesController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = current_user
+  end
 
   def update_user_params
     params.require(:user).permit(:user_name, :email, :phone_number, :name, :address, :evaluation_point, :password, :introduction, :password_confirmation)
